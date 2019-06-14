e29139fd33163c231f5ca4ccdb2799353a7f8428
mobdim <dima@mobdim.com>
2019-06-14 22:48:38 +0300

    Add carthage support

---
+++ SDVersion-Demo/SDVersion/SDVersion.h
@@ -0,0 +1,19 @@
+//
+//  SDVersion.h
+//  SDVersion
+//
+//  Created by Dmitriy Zadoroghnyy on 14/06/2019.
+//  Copyright © 2019 Sebastian Dobrincu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SDVersion.
+FOUNDATION_EXPORT double SDVersionVersionNumber;
+
+//! Project version string for SDVersion.
+FOUNDATION_EXPORT const unsigned char SDVersionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDVersion/PublicHeader.h>
+
+
--- SDVersion/SDVersion.h
+++ SDVersion/SDVersion.h
@@ -8,6 +8,18 @@
 #ifndef SDVersion_h
 #define SDVersion_h
 
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SDVersion.
+FOUNDATION_EXPORT double SDVersionVersionNumber;
+
+//! Project version string for SDVersion.
+FOUNDATION_EXPORT const unsigned char SDVersionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDVersion/PublicHeader.h>
+
+
 #if TARGET_OS_IOS
 	#import "SDiOSVersion.h"
 	#define SDVersion SDiOSVersion
